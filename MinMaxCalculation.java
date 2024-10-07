60762e7051e191db8d33c6b40ef762a42cec96b0
yiming huang <yiminghuang@umass.edu>
2024-10-07 18:47:07 -0400

    update MinMaxCalculation.java

--- MinMaxCalculation.java
+++ MinMaxCalculation.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
+import Math.*;
 public class MinMaxCalculation {
+    public static double min(double ... numbers) {
+        double min = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            min = Math.min(min, numbers[i]);
+        }
+        return min;
+    }
+
+    public static double min(double ... numbers) {
+        double max = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            max = Math.max(max, numbers[i]);
+        }
+        return max;
+    }
 }
